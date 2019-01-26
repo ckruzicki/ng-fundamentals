211e0fce14d9d48b0dc39418c20f3a0966ebb505
Corey Kruzicki <corey0708@gmail.com>
2019-01-25 19:33:43 -0600

    MOD 7

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,7 @@
 import { ProfileComponent } from './profile.component';
+import { LoginComponent } from './login.component';
 
 export const userRoutes = [
-    // /user/profile
-    { path: 'profile', components: ProfileComponent}
+    { path: 'login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent } // /user/profile
 ]
